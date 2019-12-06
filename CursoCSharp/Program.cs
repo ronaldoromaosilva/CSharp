fa303745c7b331032d9db2dd1fbf5e44f98262bb
Ronaldo Romão Silva <ronaldo.romao.silva@hotmail.com>
2019-12-05 23:30:39 -0300

    Encapsulamento

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -75,6 +75,7 @@ namespace CursoCSharp {
                 //OO
                 { "Herança - OO", Heranca.Executar },
                 { "Construtor This - OO", ConstrutorThis.Executar },
+                { "Encapsulamento - OO", OO.Encapsulamento.Executar },
             });
 
 
---
+++ Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    public class SubCelebridade
+    {
+        // Todos
+        public string InfoPublica = "Tenho um instagram";
+
+        // Herança
+        protected string CorDoOlho = "Verde";
+
+        // mesmo projeto (assembly)
+        internal ulong NumeroCelular = 5511999999999;
+
+        // herança ou mesmo projeto
+        protected internal string JeitoDeFalar = "Uso muitas girias";
+
+        // mesma class ou herança no mesmo projeto (c# >= 7.2)
+        private protected string SegredoFamilia = "Bla Bla";
+
+        // private é o padrão
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("SubCelebridade...");
+
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+
+        }
+   
+    }
+}
